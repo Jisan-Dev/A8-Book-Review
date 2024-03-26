47bff785817bb5a9b7785e864d98d37a025381ad
Jisun696 <istiakkashem35@gmail.com>
2024-03-27 03:14:08 +0600

    added a UI component 'Button'

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,17 +1,24 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-// const Button = ({ text, bg, textSize }) => {
-//   return (
-//     <>
-//       <button className={`cursor-pointer rounded-lg ${bg} text-white text-[14px] md:text-[18px] font-semibold py-3 md:py-[18px] px-4 md:px-7`}>Sign In</button>
-//     </>
-//   );
-// };
+const Button = ({ text, bg, textSize, px, py }) => {
+  return (
+    <>
+      <button
+        className={`cursor-pointer rounded-lg ${bg ? bg : 'bg-primary'} text-white ${textSize ? textSize : 'text-[18px]'} font-semibold ${px ? px : 'px-7'} ${
+          py ? py : 'py-[18px]'
+        }`}>
+        {text}
+      </button>
+    </>
+  );
+};
 
-// Button.propTypes = {
-//   text: PropTypes.string,
-//   bg: PropTypes.string,
-//   textSize: PropTypes.string,
-// };
+Button.propTypes = {
+  text: PropTypes.string,
+  bg: PropTypes.string,
+  textSize: PropTypes.string,
+  px: PropTypes.string,
+  py: PropTypes.string,
+};
 
-// export default Button;
+export default Button;
