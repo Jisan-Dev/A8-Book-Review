7cccade82f3e51eb00253f158b692da5bb0f006e
Jisun696 <istiakkashem35@gmail.com>
2024-03-28 03:22:06 +0600

    listed books page completed with functionalities

---
+++ src/components/listed-book-card/ListedBookCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { GrLocation } from 'react-icons/gr';
+import { HiOutlineUsers } from 'react-icons/hi2';
+import { MdOutlineDescription } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
+const ListedBookCard = ({ book }) => {
+  return (
+    <div className="p-6 flex gap-6 border rounded-2xl mb-6">
+      <div className="bg-neutral-900 bg-opacity-5 rounded-2xl flex items-center justify-center w-[230px]">
+        <img src={book.image} className="object-contain h-[172px]" />
+      </div>
+      <div>
+        <h1 className="text-neutral-900 text-2xl font-bold font-playFair mb-4">{book.bookName}</h1>
+        <h2 className="text-neutral-900 text-opacity-80 text-base font-medium mb-5">By : {book.author}</h2>
+        <div className="flex items-center gap-3 mb-6">
+          <span className="text-neutral-900 text-base font-bold mr-2 ">Tags</span>
+          {book.tags.map((tag, id) => (
+            <span key={id} className="text-primary bg-[#23BE0A0D] px-4 py-2 mr-3 rounded-full text-base font-medium">
+              #{tag}
+            </span>
+          ))}
+          <span className="flex items-center gap-2 text-neutral-900 text-opacity-80 text-base font-normal">
+            <GrLocation className="text-lg" /> Year of Publishing : {book.yearOfPublishing}
+          </span>
+        </div>
+        <div className="flex gap-4">
+          <span className="flex gap-2 items-center text-neutral-900 text-opacity-60 text-base font-normal">
+            <HiOutlineUsers className="text-xl" /> Publisher: {book.yearOfPublishing}
+          </span>
+          <span className="flex gap-2 items-center text-neutral-900 text-opacity-60 text-base font-normal">
+            <MdOutlineDescription className="text-xl" /> Page {book.totalPages}
+          </span>
+        </div>
+        <div className="border-t border-neutral-900 border-opacity-20 pt-5 mt-5">
+          <span className="text-[#328EFF] bg-[#328EFF26] px-4 py-[10px] mr-3 rounded-full text-base font-medium">Category: {book.category}</span>
+          <span className="text-[#FFAC33] bg-[#FFAC3326] px-4 py-[10px] mr-3 rounded-full text-base font-medium">Rating: {book.rating}</span>
+          <Link to={`/books/${book.bookId}`}>
+            <button className="bg-primary rounded-full text-white py-[10px] px-[20px] text-base">View Details</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ListedBookCard.propTypes = {
+  book: PropTypes.object.isRequired,
+};
+
+export default ListedBookCard;
--- src/pages/listed-books/ListedBooks.jsx
+++ src/pages/listed-books/ListedBooks.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { getStoredReadBooks, getStoredWishedBooks } from '../../utils/localstorage';
 import { useLoaderData } from 'react-router-dom';
+import ListedBookCard from '../../components/listed-book-card/ListedBookCard';
+import { FaChevronDown } from 'react-icons/fa';
 
 const ListedBooks = () => {
   const allBooks = useLoaderData();
   const storedReadBooks = getStoredReadBooks();
   const storedWishedBooks = getStoredWishedBooks();
-  const readBooks = allBooks.filter((book) => storedReadBooks.includes(book.bookId));
-  const wishedBooks = allBooks.filter((book) => storedWishedBooks.includes(book.bookId));
+  const readBooksFiltered = allBooks.filter((book) => storedReadBooks.includes(book.bookId));
+  const [readBooks, setReadBooks] = useState(readBooksFiltered);
+  const wishedBooksFiltered = allBooks.filter((book) => storedWishedBooks.includes(book.bookId));
+  const [wishedBooks, setWishedBooks] = useState(wishedBooksFiltered);
+
+  const sortReadBooks = (field) => {
+    const sortedBooks = [...readBooks].sort((a, b) => {
+      if (a[field] < b[field]) return -1;
+      if (a[field] > b[field]) return 1;
+      return 0;
+    });
+    setReadBooks(sortedBooks);
+    const sortedWished = [...wishedBooks].sort((a, b) => {
+      if (a[field] < b[field]) return -1;
+      if (a[field] > b[field]) return 1;
+      return 0;
+    });
+    setWishedBooks(sortedWished);
+  };
+
   return (
     <div className="container mx-auto font-workSans">
+      <div className="h-[100px] bg-neutral-900 bg-opacity-5 flex items-center justify-center rounded-2xl mb-8">
+        <h1 className="text-neutral-900 text-[28px] font-bold">Books</h1>
+      </div>
+      <div className="flex justify-center">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1 w-48 bg-primary text-white hover:bg-primary">
+            Sort By <FaChevronDown />
+          </div>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 ">
+            <li className="items-center" onClick={() => sortReadBooks('rating')}>
+              <a>Rating</a>
+            </li>
+            <li className="items-center" onClick={() => sortReadBooks('totalPages')}>
+              <a>Number of pages</a>
+            </li>
+            <li className="items-center" onClick={() => sortReadBooks('yearOfPublishing')}>
+              <a>Publisher Year</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
@@ -20,12 +61,12 @@ const ListedBooks = () => {
 
         <TabPanel>
           {readBooks.map((book, id) => (
-            <h1 key={id}>{book.bookName}</h1>
+            <ListedBookCard key={id} book={book} />
           ))}
         </TabPanel>
         <TabPanel>
           {wishedBooks.map((book, id) => (
-            <h1 key={id}>{book.bookName}</h1>
+            <ListedBookCard key={id} book={book} />
           ))}
         </TabPanel>
       </Tabs>
