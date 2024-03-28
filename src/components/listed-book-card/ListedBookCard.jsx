158869e96bcfdc6827b991e15addbd3c1326994a
Jisun696 <istiakkashem35@gmail.com>
2024-03-28 06:47:13 +0600

    made existing pages responsive

--- src/components/listed-book-card/ListedBookCard.jsx
+++ src/components/listed-book-card/ListedBookCard.jsx
@@ -7,14 +7,14 @@ import { Link } from 'react-router-dom';
 
 const ListedBookCard = ({ book }) => {
   return (
-    <div className="p-6 flex gap-6 border rounded-2xl mb-6">
-      <div className="bg-neutral-900 bg-opacity-5 rounded-2xl flex items-center justify-center w-[230px]">
+    <div className="p-6 flex flex-col md:flex-row gap-6 border rounded-2xl mb-6">
+      <div className="bg-neutral-900 bg-opacity-5 rounded-2xl flex items-center justify-center w-[230px] max-sm:w-full max-sm:h-64">
         <img src={book.image} className="object-contain h-[172px]" />
       </div>
       <div>
         <h1 className="text-neutral-900 text-2xl font-bold font-playFair mb-4">{book.bookName}</h1>
         <h2 className="text-neutral-900 text-opacity-80 text-base font-medium mb-5">By : {book.author}</h2>
-        <div className="flex items-center gap-3 mb-6">
+        <div className="flex items-center gap-3 mb-6 flex-wrap">
           <span className="text-neutral-900 text-base font-bold mr-2 ">Tags</span>
           {book.tags.map((tag, id) => (
             <span key={id} className="text-primary bg-[#23BE0A0D] px-4 py-2 mr-3 rounded-full text-base font-medium">
@@ -33,7 +33,7 @@ const ListedBookCard = ({ book }) => {
             <MdOutlineDescription className="text-xl" /> Page {book.totalPages}
           </span>
         </div>
-        <div className="border-t border-neutral-900 border-opacity-20 pt-5 mt-5">
+        <div className="border-t border-neutral-900 border-opacity-20 pt-5 mt-5 max-sm:flex max-sm:flex-wrap gap-2 ">
           <span className="text-[#328EFF] bg-[#328EFF26] px-4 py-[10px] mr-3 rounded-full text-base font-medium">Category: {book.category}</span>
           <span className="text-[#FFAC33] bg-[#FFAC3326] px-4 py-[10px] mr-3 rounded-full text-base font-medium">Rating: {book.rating}</span>
           <Link to={`/books/${book.bookId}`}>
