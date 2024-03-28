4bf94f4fb6c9c911804f0a86636787d07c2bd7dd
Jisun696 <istiakkashem35@gmail.com>
2024-03-28 07:07:44 +0600

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# B9A8-Book-Review
+# Read_React
 
-## [ Private Repo Link](https://classroom.github.com/a/3k0P1rCt)
+## your favorite book review website.
 
-Click here for the private repo: [https://classroom.github.com/a/3k0P1rCt](https://classroom.github.com/a/3k0P1rCt)
+### What's Special?
+
+1. **Diverse and Engaging Content:** Offer a variety of book reviews, from current bestsellers to hidden gems and classics.
+2. **Curated Lists:** Feature curated lists for different genres, interests, or reading goals
+3. **Beautiful and User-Friendly Design:** Ensure a clean, modern layout with easy navigation. Use high-quality book covers and eye-friendly color schemes.
+4. **Mobile Responsiveness:** Make sure your website is optimized for mobile devices to cater to on-the-go readers.
+5. **Graph View:** You can see your read list books pages in a graph view that makes your decision making easier.
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import Button from '../../components/ui/Button';
 import bannerImage from '../../assets/images/bannerBook.png';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import HomeBookCard from '../../components/home-book-card/HomeBookCard';
 
 const Home = () => {
@@ -12,7 +12,9 @@ const Home = () => {
       <section className="container mx-auto bg-[#1313130D] px-4 md:px-[120px] py-12 md:py-20 flex flex-col md:flex-row items-center justify-between rounded-3xl mt-3 md:mt-6">
         <div className="max-w-[526px] ">
           <h1 className="text-neutral-900 text-5xl md:text-[56px] font-bold font-playFair mb-10 max-sm:leading-tight">Books to freshen up your bookshelf</h1>
-          <Button text="View The List" />
+          <Link to={'/listed-books'}>
+            <Button text="View The List" />
+          </Link>
         </div>
         <div>
           <img src={bannerImage} alt="" />
